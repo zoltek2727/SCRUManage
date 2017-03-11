d5704374db0df665269af4b283e7173b0fe1027f
barpaw <barpaw@gmail.com>
2017-03-11 19:30:07 +0100

    index controller

--- SCRUManage/config/routes.rb
+++ SCRUManage/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
 end
