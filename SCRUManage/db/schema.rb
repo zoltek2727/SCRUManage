31132f4f5a6eec63103d654be35a4265e1bca25b
zoltek2727 <zoltek2727@wp.pl>
2017-04-03 22:32:07 +0200

    Utworzona tabela projekty, testowanie dodwania tabel

--- SCRUManage/db/schema.rb
+++ SCRUManage/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311175307) do
+ActiveRecord::Schema.define(version: 20170403200046) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "StartDate"
+    t.datetime "EndDate"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
