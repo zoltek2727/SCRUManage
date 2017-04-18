0ad5ab2227af048ff5d4a5061f61077654df08f8
barpaw <barpaw@gmail.com>
2017-04-18 22:02:22 +0200

    seeds

--- SCRUManage/db/seeds.rb
+++ SCRUManage/db/seeds.rb
@@ -6,13 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Project.create(name: 'ProjektNASA', description: 'Projekt tworzony dla NASA', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
-#Project.create(name: 'Projekt w Ruby on Rails', description: 'Projekt tworzony w czymś o czym nie mamy pojęcia', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
-#Project.create(name: 'Projekt grupowy z czego 2 robi a 2 śpi', description: 'Czterech jeźdzców apokalipsy', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
+#Project.create(name: 'Projekt NASA', description: 'Projekt tworzony dla NASA', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
+#Project.create(name: 'Projekt w Ruby on Rails', description: 'Projekt tworzony w czymś o czym się uczymy', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
+#Project.create(name: 'Projekt grupowy', description: 'Czterech jeźdzców apokalipsy', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
+#Project.create(name: 'Projekt dla studentów', description: 'Uwaga to pułapka', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date)
 
-#Sprint.create(sprintName: 'Sprint1', sprintDescription: 'Logowanie', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 2)
-#Sprint.create(sprintName: 'Sprint2', sprintDescription: 'Projekty', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 2)
-#Sprint.create(sprintName: 'Sprint1,1', sprintDescription: 'no nie wiem co', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 1)
+#Sprint.create(sprintName: 'Sprint1', sprintDescription: 'Logowanie do modułu', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 2)
+#Sprint.create(sprintName: 'Sprint2', sprintDescription: 'rejestracja na stronie', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 2)
+#Sprint.create(sprintName: 'Sprint3', sprintDescription: 'Migracja danych', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 1)
+#Sprint.create(sprintName: 'Sprint4', sprintDescription: 'Konfiguracja serwera', StartDate: DateTime.now.to_date, EndDate: DateTime.now.to_date, project_id: 3)
 
 #ProjectRole.create(projRoleName: 'programista')
 #ProjectRole.create(projRoleName: 'sieciowiec')
